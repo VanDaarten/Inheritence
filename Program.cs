2606ddfb61e521a92515d4ea4d567b29f2924a2f
VanDaarten <van.pandiangan@students.amikom.ac.id>
2019-05-07 14:52:44 +0700

    Inheritance

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "Van";
+            person.Age = 20;
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher();
+            teacher.Name = " Daarten";
+            teacher.Age = 21;
+            teacher.TeacherId = "2278";
+            teacher.Subject = "IPA";
+            teacher.GetNameAndAge();
+
+            Student student = new Student();
+            student.Name = "Pandiangan";
+            student.Age = 22;
+            student.StudentId = "18.11.2278";
+            student.Email = "pandiangan19@amikom.ac.id";
+            student.GetNameAndAge();
+
+            Console.ReadKey();
+        }
+    }
+}
